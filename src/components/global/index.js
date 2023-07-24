82861a926bcfb53109205865bfb853dbd9efeb22
Oleksii <alex@nerder.com>
2023-07-23 20:40:31 -0600

    Tomorrow Coding Assessment

---
+++ src/components/global/index.js
@@ -0,0 +1,6 @@
+export {Drawer, useDrawer} from './Drawer.client';
+export {FooterMenu} from './FooterMenu.client';
+export {Header} from './Header.client';
+export {Modal} from './Modal.client';
+export {PageHeader} from './PageHeader';
+export {SortFilter} from './SortFilter.client';
---
+++ src/routes/collections/[handle].server.jsx
@@ -0,0 +1,288 @@
+import {Suspense} from 'react';
+import {
+  gql,
+  Seo,
+  ShopifyAnalyticsConstants,
+  useServerAnalytics,
+  useLocalization,
+  useShopQuery,
+  useUrl,
+} from '@shopify/hydrogen';
+
+import {PRODUCT_CARD_FRAGMENT} from '~/lib/fragments';
+import {PageHeader, ProductGrid, Section, Text, SortFilter} from '~/components';
+import {NotFound, Layout} from '~/components/index.server';
+
+const pageBy = 48;
+
+export default function Collection({params}) {
+  const {handle} = params;
+  const knownFilters = ['productVendor', 'productType'];
+  const available = 'available';
+  const variantOption = 'variantOption';
+  const url = new URL(useUrl());
+  const searchParams = new URLSearchParams(url.search);
+  const {sortKey, reverse} = getSortValuesFromParam(searchParams.get('sort'));
+  const filters = [];
+  const appliedFilters = [];
+
+  for (const [key, value] of searchParams.entries()) {
+    if (available === key) {
+      filters.push({available: value === 'true'});
+      appliedFilters.push({
+        label: value === 'true' ? 'In stock' : 'Out of stock',
+        urlParam: {
+          key: available,
+          value,
+        },
+      });
+    } else if (knownFilters.includes(key)) {
+      filters.push({[key]: value});
+      appliedFilters.push({label: value, urlParam: {key, value}});
+    } else if (key.includes(variantOption)) {
+      const [name, val] = value.split(':');
+      filters.push({variantOption: {name, value: val}});
+      appliedFilters.push({label: val, urlParam: {key, value}});
+    }
+  }
+  if (searchParams.has('minPrice') || searchParams.has('maxPrice')) {
+    const price = {};
+    if (searchParams.has('minPrice')) {
+      price.min = Number(searchParams.get('minPrice')) || 0;
+      appliedFilters.push({
+        label: `Min: $${price.min}`,
+        urlParam: {key: 'minPrice', value: searchParams.get('minPrice')},
+      });
+    }
+    if (searchParams.has('maxPrice')) {
+      price.max = Number(searchParams.get('maxPrice')) || 0;
+      appliedFilters.push({
+        label: `Max: $${price.max}`,
+        urlParam: {key: 'maxPrice', value: searchParams.get('maxPrice')},
+      });
+    }
+    filters.push({
+      price,
+    });
+  }
+  const {
+    language: {isoCode: language},
+    country: {isoCode: country},
+  } = useLocalization();
+
+  const {
+    data: {collection, collections},
+  } = useShopQuery({
+    query: COLLECTION_QUERY,
+    variables: {
+      handle,
+      filters,
+      sortKey,
+      reverse,
+      language,
+      country,
+      pageBy,
+    },
+    preload: true,
+  });
+
+  if (!collection) {
+    return <NotFound type="collection" />;
+  }
+
+  useServerAnalytics({
+    shopify: {
+      pageType: ShopifyAnalyticsConstants.pageType.collection,
+      resourceId: collection.id,
+    },
+  });
+
+  return (
+    <Layout>
+      <Suspense>
+        <Seo type="collection" data={collection} />
+      </Suspense>
+      <PageHeader heading={collection.title}>
+        {collection?.description && (
+          <div className="flex items-baseline justify-between w-full">
+            <div>
+              <Text format width="narrow" as="p" className="inline-block">
+                {collection.description}
+              </Text>
+            </div>
+          </div>
+        )}
+      </PageHeader>
+      <SortFilter
+        filters={collection.products.filters}
+        appliedFilters={appliedFilters}
+        collections={collections}
+      />
+      <Section>
+        <ProductGrid
+          key={collection.id}
+          collection={collection}
+          url={`/collections/${handle}?country=${country}`}
+        />
+      </Section>
+    </Layout>
+  );
+}
+
+// API endpoint that returns paginated products for this collection
+// @see templates/demo-store/src/components/product/ProductGrid.client.tsx
+export async function api(request, {params, queryShop}) {
+  if (request.method !== 'POST') {
+    return new Response('Method not allowed', {
+      status: 405,
+      headers: {Allow: 'POST'},
+    });
+  }
+  const url = new URL(request.url);
+
+  const cursor = url.searchParams.get('cursor');
+  const country = url.searchParams.get('country');
+  const {handle} = params;
+
+  return await queryShop({
+    query: PAGINATE_COLLECTION_QUERY,
+    variables: {
+      handle,
+      cursor,
+      pageBy,
+      country,
+    },
+  });
+}
+
+const COLLECTION_QUERY = gql`
+  ${PRODUCT_CARD_FRAGMENT}
+  query CollectionDetails(
+    $handle: String!
+    $country: CountryCode
+    $language: LanguageCode
+    $filters: [ProductFilter!]
+    $sortKey: ProductCollectionSortKeys!
+    $reverse: Boolean
+    $pageBy: Int!
+    $cursor: String
+  ) @inContext(country: $country, language: $language) {
+    collection(handle: $handle) {
+      id
+      title
+      description
+      seo {
+        description
+        title
+      }
+      image {
+        id
+        url
+        width
+        height
+        altText
+      }
+      products(
+        first: $pageBy
+        after: $cursor
+        filters: $filters
+        sortKey: $sortKey
+        reverse: $reverse
+      ) {
+        filters {
+          id
+          label
+          type
+          values {
+            id
+            label
+            count
+            input
+          }
+        }
+        nodes {
+          ...ProductCard
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+  }
+`;
+
+const PAGINATE_COLLECTION_QUERY = gql`
+  ${PRODUCT_CARD_FRAGMENT}
+  query CollectionPage(
+    $handle: String!
+    $pageBy: Int!
+    $cursor: String
+    $country: CountryCode
+    $language: LanguageCode
+  ) @inContext(country: $country, language: $language) {
+    collection(handle: $handle) {
+      products(
+        first: $pageBy
+        after: $cursor
+        filters: $filters
+        sortKey: $sortKey
+        reverse: $reverse
+      ) {
+        filters {
+          id
+          label
+          type
+          values {
+            id
+            label
+            count
+            input
+          }
+        }
+        nodes {
+          ...ProductCard
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+  }
+`;
+
+function getSortValuesFromParam(sortParam) {
+  switch (sortParam) {
+    case 'price-high-low':
+      return {
+        sortKey: 'PRICE',
+        reverse: true,
+      };
+    case 'price-low-high':
+      return {
+        sortKey: 'PRICE',
+        reverse: false,
+      };
+    case 'best-selling':
+      return {
+        sortKey: 'BEST_SELLING',
+        reverse: false,
+      };
+    case 'newest':
+      return {
+        sortKey: 'CREATED',
+        reverse: true,
+      };
+    case 'featured':
+      return {
+        sortKey: 'MANUAL',
+        reverse: false,
+      };
+    default:
+      return {
+        sortKey: 'RELEVANCE',
+        reverse: false,
+      };
+  }
+}
